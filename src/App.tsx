cba851dcab85368351a67d5da6e6407e37a1e3cd
dark <473380917@qq.com>
2021-05-03 15:16:44 +0800

    config: tsconfig.json support .jsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React,{FC} from 'react'
+
+interface Props{
+
+}
+
+const App:FC<Props> = (props)=>{
+  
+  return (<div>Hello</div>)
+}
+
+export default App
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-console.log('hello');
-const sum = (a:number,b:number)=>{
-  return a+b
-}
-class Animal {}
-const animal = new Animal()
-console.log(sum(1, 2));
