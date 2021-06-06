e5461ff081a53471a5248f2d8040756ed71c45dd
dark <473380917@qq.com>
2021-06-06 22:43:44 +0800

    支持eslint

--- snowpack.config.js
+++ snowpack.config.js
@@ -24,6 +24,14 @@ module.exports = {
         },
       },
     ],
+    [
+      '@snowpack/plugin-run-script',
+      {
+        cmd: 'eslint src --ext .js,.jsx,.ts,.tsx',
+        // Optional: Use npm package "eslint-watch" to run on every file change
+        watch: 'esw -w --clear src --ext .js,.jsx,.ts,.tsx',
+      },
+    ],
   ],
   packageOptions: {
     /* ... */
